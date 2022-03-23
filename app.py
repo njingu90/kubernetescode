3660d0fcc90e87f6c7409f2c6d4a7df300e034e3
njingu90 <85954427+njingu90@users.noreply.github.com>
2022-03-22 22:34:05 -0600

    Update app.py

--- app.py
+++ app.py
@@ -2,9 +2,5 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-def index():
-    return 'testingimage'
-
-@app.route('/hello')
-def hello():
-    return 'Hello, World!!!'
+def hello_world():
+    return 'test1!!!'
